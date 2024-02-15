7066dddbba1765b858a341ef1e259b4197999029
5bh4rdwaj <samvatb@protonmail.com>
2024-02-15 17:20:33 +0530

    string concat and list manupilation file

---
+++ newtest.py
@@ -0,0 +1,15 @@
+import random
+# Wage war by all means
+
+# accepting list of locations
+locations = ['rome', 'paris' , 'madrid', 'Ulaanbaatar', 'Helsinki'] 
+activities = ['wine tasting', 'talk a walk', 'coffee dates' , "Couple's Horse Riding"]
+
+# random choice (lesggoo)
+location_chosen= random.choice(locations)
+activities_chosen = random.choice(activities)
+
+# Asking her out here
+print ("Hey sweetheart might you be interested in accompanying me to " + location_chosen + " and indulging yourself in some " + activities_chosen + "?")
+
+# Jai Hind 
